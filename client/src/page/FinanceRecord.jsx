7761908b65bb61723e3a63c1cd91c035196eaa1c
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-10-04 20:36:06 +0700

    Reset Client React

--- client/src/page/FinanceRecord.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Nav from "../component/Nav";
-
-function FinanceRecord(){
-    return (
-        <><Nav /><div>Finance Record</div></>
-    )
-}
-export default FinanceRecord
--- client/src/page/History.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Nav from "../component/Nav";
-
-function History(){
-    return (
-        <><Nav /><div>History</div></>
-    )
-}
-export default History
