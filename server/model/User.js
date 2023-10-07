acb7067186520381b197d41143b2d1cd8a368d82
thitiphon.s <colonp.pth@gmail.com>
2023-10-07 11:53:07 +0700

    Update User.js

--- server/model/User.js
+++ server/model/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    first_name: {type: String, default: null},
-    last_name: {type: String, default: null},
-    email: {type: String, unique: true},
-    pwd: {type: String},
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
+    pwd: {type: String, required: true},
     Expense: [
         {
             money: {type: Number},
